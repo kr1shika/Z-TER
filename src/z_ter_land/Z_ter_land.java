b22cf05b1c8595ab474a75f779283d08b07c0a53
krishika <krishikakh@gmail.com>
2023-06-08 15:01:31 +0545

    landing page ongoing

---
+++ src/z_ter_land/Z_ter_land.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package z_ter_land;
+
+/**
+ *
+ * @author Shishir
+ */
+public class Z_ter_land {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
